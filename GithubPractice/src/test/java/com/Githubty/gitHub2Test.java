afd27c2a8c34e6961bc6195f88b33c1db4974e4a
dell-pc <dell-pc@dell>
2021-11-12 17:47:56 +0530

    eith commit

--- GithubPractice/src/test/java/com/Githubty/gitHub2Test.java
+++ GithubPractice/src/test/java/com/Githubty/gitHub2Test.java
@@ -7,6 +7,10 @@ public class gitHub2Test
 	@Test
 	public void demo2()
 	{
+		String browser = System.getProperty("browser");
+		String url = System.getProperty("url");
+		System.out.println(browser);
+		System.out.println(url);
 		System.out.println("second commit");
 		System.out.println("hello");
 		System.out.println("gudeveng");
